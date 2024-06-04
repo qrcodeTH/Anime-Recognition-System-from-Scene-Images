de3d39259867ad7fdc9106f91b1a271c285f0c58
qrcodeTH <139485424+qrcodeTH@users.noreply.github.com>
2024-06-04 16:42:13 +0700

    Add files via upload

---
+++ Classification/Classify_Efficientnet-b0.py
@@ -0,0 +1,150 @@
+#!pip install  efficientnet_pytorch
+
+import os
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import torch
+from torch import optim
+from loss.labelloss import LabelSmoothing  
+import torch.nn as nn
+from efficientnet_pytorch import EfficientNet
+
+class MyEfficientNet(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        # EfficientNet
+        self.network = EfficientNet.from_pretrained("efficientnet-b0")
+        
+        self.network._fc = nn.Linear(self.network._fc.in_features, 512)
+        self.classifier = nn.Linear(512,100)
+    def forward(self, x):
+        out = self.network(x)
+        return out
+    
+# Set CUDA_VISIBLE_DEVICES environment variable
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+
+# Function for training and validation
+def train_val(start_epoch=0, resume_path=None):
+    model.train()
+    best_acc = 0.0
+    checkpoint_files = []
+
+    if resume_path:
+        checkpoint = torch.load(resume_path)
+
+        if 'model_state_dict' in checkpoint and 'optimizer_state_dict' in checkpoint:
+            model.load_state_dict(checkpoint['model_state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            start_epoch = checkpoint['epoch'] + 1
+            print(f'Resuming training from epoch {start_epoch}')
+        else:
+            model.load_state_dict(checkpoint)
+            start_epoch = 1
+            print(f'Resuming training from epoch {start_epoch} with direct model state loading.')
+    else:
+        print('Starting training from scratch')
+
+    for epoch in range(start_epoch, epochs):
+        print(f'Epoch {epoch + 1}/{epochs}')
+        train_loss = 0.0
+        train_count = 0
+        model.train()
+        for inputs, labels in tqdm(trainloader):
+            inputs, labels = inputs.cuda(), labels.cuda()
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            _, preds = torch.max(outputs, 1)
+            train_loss += loss.item() * inputs.size(0)
+            train_count += torch.sum(labels == preds)
+        train_loss /= len(trainset)
+        train_acc = train_count.double() / len(trainset)
+
+        print(f'Train loss: {train_loss:.4f}, Train acc: {train_acc:.4f}')
+
+        val_loss = 0.0
+        val_count = 0
+        model.eval()
+        with torch.no_grad():
+            for inputs, labels in tqdm(valloader):
+                inputs, labels = inputs.cuda(), labels.cuda()
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                _, preds = torch.max(outputs, 1)
+                val_loss += loss.item() * inputs.size(0)
+                val_count += torch.sum(labels == preds)
+            val_loss /= len(valset)
+            val_acc = val_count.double() / len(valset)
+            
+            print(f'Val loss: {val_loss:.4f}, Val acc: {val_acc:.4f}')
+            
+            if best_acc < val_acc:
+                best_acc = val_acc
+                torch.save(model.state_dict(), './best_model.pth')
+                print(f'Saving best model with accuracy: {val_acc:.4f}')
+
+            # Save checkpoint
+            checkpoint_filename = f'./checkpoint_epoch_{epoch + 1}.pth'
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': loss,
+            }, checkpoint_filename)
+
+            checkpoint_files.append(checkpoint_filename)
+            if len(checkpoint_files) > 3:
+                old_checkpoint = checkpoint_files.pop(0)
+                if os.path.exists(old_checkpoint):
+                    os.remove(old_checkpoint)
+
+            with open('./training_log.txt', 'a') as f:
+                f.write(f'Epoch {epoch + 1}/{epochs}\n')
+                f.write(f'Train loss: {train_loss:.4f}, Train acc: {train_acc:.4f}\n')
+                f.write(f'Val loss: {val_loss:.4f}, Val acc: {val_acc:.4f}\n\n')
+
+# Parameters
+lr = 0.01
+epochs = 100
+smoothing = 0.1
+batch_size = 128
+trainpath = './Train'
+valpath = './Validation'
+resume_path = None
+
+# Data transformations 
+train_transforms = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.RandomCrop((224, 224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+val_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# Load datasets and create data loaders
+trainset = datasets.ImageFolder(trainpath, transform=train_transforms)
+trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+valset = datasets.ImageFolder(valpath, transform=val_transforms)
+valloader = DataLoader(valset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+# Initialize model, loss criterion, and optimizer
+model = MyEfficientNet().cuda()
+criterion = LabelSmoothing(smoothing=smoothing).cuda()
+optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=0, nesterov=False)
+
+# Start training and validation
+train_val(start_epoch=0, resume_path=resume_path)
