5769818eb33e903a271b7145a2ac571d68c1f207
qrcodeTH <139485424+qrcodeTH@users.noreply.github.com>
2024-06-05 23:34:38 +0700

    Add files via upload

---
+++ model/model (1).py
@@ -0,0 +1,57 @@
+import torch.nn as nn
+from efficientnet_pytorch import EfficientNet
+import torchvision.models as models
+
+class MyEfficientNetB0(nn.Module):
+    def __init__(self):
+        super(MyEfficientNetB0, self).__init__()
+        self.network = EfficientNet.from_pretrained("efficientnet-b0")
+        self.network._fc = nn.Linear(self.network._fc.in_features, 512)
+        self.classifier = nn.Linear(512, 100)
+
+    def forward(self, x):
+        out = self.network(x)
+        return out
+
+class MyEfficientNetB7(nn.Module):
+    def __init__(self):
+        super(MyEfficientNetB7, self).__init__()
+        self.network = EfficientNet.from_pretrained("efficientnet-b7")
+        self.network._fc = nn.Linear(self.network._fc.in_features, 512)
+        self.classifier = nn.Linear(512, 100)
+
+    def forward(self, x):
+        out = self.network(x)
+        return out
+
+class MyResNeXt101(nn.Module):
+    def __init__(self, num_classes=100):
+        super(MyResNeXt101, self).__init__()
+        self.network = models.resnext101_32x8d(pretrained=True)
+        in_features = self.network.fc.in_features
+        self.network.fc = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        return self.network(x)
+
+class MySEResNeXt101(nn.Module):
+    def __init__(self, num_classes=100):
+        super(MySEResNeXt101, self).__init__()
+        self.network = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_se_resnext101_32x4d', pretrained=True)
+
+        in_features = self.network.fc.in_features
+        self.network.fc = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        return self.network(x)
+
+class MyMobileNetV2(nn.Module):
+    def __init__(self, num_classes=100):
+        super(MyMobileNetV2, self).__init__()
+        self.network = models.mobilenet_v2(pretrained=True)
+
+        in_features = self.network.classifier[1].in_features
+        self.network.classifier[1] = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        return self.network(x)
