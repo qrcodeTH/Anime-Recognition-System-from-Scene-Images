8b79fa25779bd549e267476d0936a52124b6afbb
qrcodeTH <139485424+qrcodeTH@users.noreply.github.com>
2024-06-05 23:07:28 +0700

    Update feature_extraction.py

--- Retrieval/feature_extraction.py
+++ Retrieval/feature_extraction.py
@@ -7,17 +7,7 @@ import torch
 from torchvision import transforms
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-
-class MyResNeXt101(nn.Module):
-    def __init__(self, num_classes=100):
-        super(MyResNeXt101, self).__init__()
-        self.network = models.resnext101_32x8d(pretrained=True)
-
-        in_features = self.network.fc.in_features
-        self.network.fc = nn.Linear(in_features, num_classes)
-
-    def forward(self, x):
-        return self.network(x)
+from Classify_MobileNetV2 import MyResNeXt101
 
 class MyDataset(Dataset):
     def __init__(self, dirs, labels, transform=None):
