93695eec0cb67beefb0cd800228565addcbc05c6
qrcodeTH <139485424+qrcodeTH@users.noreply.github.com>
2024-06-05 23:19:25 +0700

    Update model.py

--- Classification/model.py
+++ Classification/model.py
@@ -33,3 +33,25 @@ class MyResNeXt101(nn.Module):
 
     def forward(self, x):
         return self.network(x)
+
+class MySEResNeXt101(nn.Module):
+    def __init__(self, num_classes=100):
+        super(MySEResNeXt101, self).__init__()
+        self.network = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_se_resnext101_32x4d', pretrained=True)
+
+        in_features = self.network.fc.in_features
+        self.network.fc = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        return self.network(x)
+
+class MyMobileNetV2(nn.Module):
+    def __init__(self, num_classes=100):
+        super(MyMobileNetV2, self).__init__()
+        self.network = models.mobilenet_v2(pretrained=True)
+
+        in_features = self.network.classifier[1].in_features
+        self.network.classifier[1] = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        return self.network(x)
