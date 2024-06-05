5e7b1d229a0b32aba578fd6c5c4af711e375b5d9
qrcodeTH <139485424+qrcodeTH@users.noreply.github.com>
2024-06-05 23:18:27 +0700

    Update Classify_Efficientnet-b0.py

--- Classification/Classify_Efficientnet-b0.py
+++ Classification/Classify_Efficientnet-b0.py
@@ -1,17 +1,35 @@
 import torch.nn as nn
 from efficientnet_pytorch import EfficientNet
+import torchvision.models as models
 
-class MyEfficientNet(nn.Module):
-
+class MyEfficientNetB0(nn.Module):
     def __init__(self):
-        super().__init__()
-
-        # EfficientNet
+        super(MyEfficientNetB0, self).__init__()
         self.network = EfficientNet.from_pretrained("efficientnet-b0")
-        
         self.network._fc = nn.Linear(self.network._fc.in_features, 512)
-        self.classifier = nn.Linear(512,100)
+        self.classifier = nn.Linear(512, 100)
+
     def forward(self, x):
         out = self.network(x)
         return out
-    
+
+class MyEfficientNetB7(nn.Module):
+    def __init__(self):
+        super(MyEfficientNetB7, self).__init__()
+        self.network = EfficientNet.from_pretrained("efficientnet-b7")
+        self.network._fc = nn.Linear(self.network._fc.in_features, 512)
+        self.classifier = nn.Linear(512, 100)
+
+    def forward(self, x):
+        out = self.network(x)
+        return out
+
+class MyResNeXt101(nn.Module):
+    def __init__(self, num_classes=100):
+        super(MyResNeXt101, self).__init__()
+        self.network = models.resnext101_32x8d(pretrained=True)
+        in_features = self.network.fc.in_features
+        self.network.fc = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        return self.network(x)
